d2de92b317b705229669b82570e7b6b69b7c9e3a
MikeCook0464 <mcook37@uwyo.edu>
2024-09-23 16:25:16 -0600

    Update cosc1010-lab03.py

--- cosc1010-lab03.py
+++ cosc1010-lab03.py
@@ -1,6 +1,6 @@
-# Your Name Here
+# Michael Cook
 # UWYO COSC 1010
-# Submission Date
+# 09/23/24
 # Lab 03 
 # Lab Section: 
 # Sources, people worked with, help given to: 
@@ -8,7 +8,7 @@
 # comments
 # here
 
-
+states = ["Wyoming", "Colorado", "Montana"]
 
 # This is your second lab section. It will primarily be based on the Introducing Lists lecture, reference it if you need
 # Complete all sections of this assignment 
@@ -21,33 +21,41 @@ print("Part One-----------------------------------------------------------------
 
 
 #print the entire list
-
+print(states)
 
 #now print the first element in the list
-
+print(states[0])
 
 #Print the last element using the syntax shown in class to access the final element (hint, think negatives)
-
+print(states[-1])
 
 #Using an F-string to access the first and second element print the string "COLORADO is south of WYOMING", matching the casing provided
-
+print(f"(states[1].upper) is south of (state[0].upper)")
 
 
 
 print("Part Two------------------------------------------------------------------------")
 #Append the following states to your list: Washington, Oregon, California and print your list
-
+states.append("Wasington")
+states.append("Oregon")
+states.append("California")
+print(states)
 
 #Again using the specific syntax mentioned in class overwrite the second to last element to be Maine, printing the list 
-
+states[-2] = ("Maine")
+print(states)
 
 #Insert the state Texas to be the third element in the list, again printing your list
-
+states.insert[2] = ("Texas")
+print(states)
 
 #Using the `del` statement remove the fourth item from the list, print your list 
-
+del states[3]
+print(states)
 
 #Remove Texas using its value, print the list
+states.remove[2]
+print(states)
 
 print("Part Three----------------------------------------------------------------------")
 #Temporarily sort your list, print it both sorted and unsorted 
